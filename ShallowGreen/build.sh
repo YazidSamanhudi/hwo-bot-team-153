14a998f003c1c7bf39c6f8d4837f843fcadd625c
Jesse Hallio <jesse@cs27000170.pp.htv.fi>
2012-09-11 03:35:40 +0300

    Switch Gson to Jackson and fix build/start.

--- ShallowGreen/build.sh
+++ ShallowGreen/build.sh
@@ -4,4 +4,8 @@ if [ ! -d build ]; then
 	mkdir build
 fi
 
-javac -cp google-gson-2.2.2/gson-2.2.2.jar -d build `find src -name "*.java" -type f -print`
+JACKSON=jackson-2.0.6/jackson-core-2.0.6.jar:jackson-2.0.6/jackson-annotations-2.0.6.jar:jackson-2.0.6/jackson-databind-2.0.6.jar
+LOGBACK=logback-1.0.7/logback-classic-1.0.7.jar:logback-1.0.7/logback-core-1.0.7.jar
+SLF4J=slf4j-1.7.0/slf4j-api-1.7.0.jar
+
+javac -cp $SLF4J:$LOGBACK:$JACKSON -d build `find src -name "*.java" -type f -print`
--- ShallowGreen/start.sh
+++ ShallowGreen/start.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-java -cp build:google-gson-2.2.2/gson-2.2.2.jar shallowgreen.ShallowGreen $*
+JACKSON=jackson-2.0.6/jackson-core-2.0.6.jar:jackson-2.0.6/jackson-annotations-2.0.6.jar:jackson-2.0.6/jackson-databind-2.0.6.jar
+LOGBACK=logback-1.0.7/logback-classic-1.0.7.jar:logback-1.0.7/logback-core-1.0.7.jar
+SLF4J=slf4j-1.7.0/slf4j-api-1.7.0.jar
+
+java -cp build:$SLF4J:$LOGBACK:$JACKSON shallowgreen.ShallowGreen $*
