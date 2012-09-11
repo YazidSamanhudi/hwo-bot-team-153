3e13dd2daab0e4b430a411222c6db8139af1c316
Jesse Hallio <jessegit@jesse.iki.fi>
2012-09-11 19:22:39 +0300

    Moved switch to Game from DogGame.

--- ShallowGreen/src/shallowgreen/Game.java
+++ ShallowGreen/src/shallowgreen/Game.java
@@ -1,8 +1,22 @@
 package shallowgreen;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import shallowgreen.message.ChangeDirMessage;
+import shallowgreen.message.ErrorMessage;
+import shallowgreen.message.GameIsOnMessage;
+import shallowgreen.message.GameIsOverMessage;
+import shallowgreen.message.GameStartedMessage;
+import shallowgreen.message.JoinMessage;
+import shallowgreen.message.JoinedMessage;
 import shallowgreen.message.Message;
+import shallowgreen.model.Update;
 
 public abstract class Game {
+	private static final Logger log=LoggerFactory.getLogger(Game.class);
 
 	protected Connection connection;
 
@@ -10,6 +24,60 @@ public abstract class Game {
 		this.connection=connection;
 	}
 
-	public abstract void handleMessage(Message message);
+//	public abstract void handleMessage(Message message);
+
+	public void handleMessage(Message message) {
+		switch(message.getMessageType()) {
+			case CHANGE_DIR:
+				changeDir((ChangeDirMessage)message);
+				break;
+			case ERROR:
+				error(((ErrorMessage)message).getMessage());
+				break;
+			case GAME_IS_ON:
+				update(((GameIsOnMessage)message).getUpdate());
+				break;
+			case GAME_IS_OVER:
+				gameIsOver(((GameIsOverMessage)message).getWinner());
+				break;
+			case GAME_STARTED:
+				gameStarted(((GameStartedMessage)message).getPlayers());
+				break;
+			case JOIN:
+				join((JoinMessage)message);
+				break;
+			case JOINED:
+				joined((JoinedMessage)message);
+				break;
+			case UNKNOWN:
+			default:
+				unknown(message);
+				// fall through
+		}
+	}
+
+	public void changeDir(ChangeDirMessage message) {
+		log.error("Server should not send change dir messages ({})",message);
+	}
+
+	public void error(String message) {
+		log.error("We did something wrong: {}",message);
+	}
+
+	public void join(JoinMessage message) {
+		log.error("Server should not send join messages ({})",message);
+	}
+
+	public void joined(JoinedMessage message) {
+		log.info("joined game, url: {}",message.getUrl());
+	}
+
+	public void unknown(Message message) {
+		log.error("Unknown message type: {}",message);
+	}
+
+	public abstract void update(Update update);
+	public abstract void gameIsOver(String winner);
+	public abstract void gameStarted(List<String> players);
 
 }
--- ShallowGreen/src/shallowgreen/game/DogGame.java
+++ ShallowGreen/src/shallowgreen/game/DogGame.java
@@ -1,16 +1,13 @@
 package shallowgreen.game;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import shallowgreen.Game;
 import shallowgreen.message.ChangeDirMessage;
-import shallowgreen.message.GameIsOnMessage;
-import shallowgreen.message.GameIsOverMessage;
-import shallowgreen.message.JoinedMessage;
-import shallowgreen.message.Message;
 import shallowgreen.model.Player;
 import shallowgreen.model.Update;
 
@@ -20,38 +17,7 @@ public class DogGame extends Game {
 	private double speed;
 
 	@Override
-	public void handleMessage(Message message) {
-		switch(message.getMessageType()) {
-			case CHANGE_DIR:
-				log.error("Server should not send change dir messages ({})",message);
-				break;
-			case ERROR:
-				log.error("We did something wrong: {}",message);
-				break;
-			case GAME_IS_ON:
-				update(((GameIsOnMessage)message).getUpdate());
-				break;
-			case GAME_IS_OVER:
-				log.info("Game over, {} won",((GameIsOverMessage)message).getWinner());
-				break;
-			case GAME_STARTED:
-				log.info("game started: {}",message);
-				break;
-			case JOIN:
-				log.error("Server should not send join messages ({})",message);
-				break;
-			case JOINED:
-				log.info("joined game, url: {}",((JoinedMessage)message).getUrl());
-				break;
-			case UNKNOWN:
-			default:
-				log.error("Unknown message type: {}",message);
-				break;
-			
-		}
-	}
-
-	private void update(Update update) {
+	public void update(Update update) {
 		Player me=update.getLeft();
 		double yDiff=(update.getBallY()+update.getBallRadius()/2)-(me.getY()+update.getPaddleHeight()/2);
 		ChangeDirMessage cdm=null;
@@ -69,6 +35,16 @@ public class DogGame extends Game {
 				// TODO Auto-generated catch block
 				log.error("Whooooops.",e);
 			}
+	}
+
+	@Override
+	public void gameIsOver(String winner) {
+		log.info("winner: {}",winner);
+	}
+
+	@Override
+	public void gameStarted(List<String> players) {
+		log.info("new game with players: {}",players);
 	}
 
 }
