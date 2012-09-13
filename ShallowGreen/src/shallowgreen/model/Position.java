6b17cbd3419b58482a7277cd597c901a754a934f
Jesse Hallio <jessegit@jesse.iki.fi>
2012-09-13 04:07:16 +0300

    Add shortcut constructor.

--- ShallowGreen/src/shallowgreen/model/Position.java
+++ ShallowGreen/src/shallowgreen/model/Position.java
@@ -5,6 +5,14 @@ public class Position {
 	private double x;
 	private double y;
 
+	public Position() {
+	}
+
+	public Position(double x, double y) {
+		this.x=x;
+		this.y=y;
+	}
+
 	public double getX() {
 		return x;
 	}
