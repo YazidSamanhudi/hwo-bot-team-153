35d0920259761cb4d678fc87267a11fe0769315f
Jesse Hallio <jessegit@jesse.iki.fi>
2012-09-16 14:42:13 +0300

    Define source encoding as UTF-8.

--- ShallowGreen/build.sh
+++ ShallowGreen/build.sh
@@ -8,5 +8,5 @@ JACKSON=jackson-2.0.6/jackson-core-2.0.6.jar:jackson-2.0.6/jackson-annotations-2
 LOGBACK=logback-1.0.7/logback-classic-1.0.7.jar:logback-1.0.7/logback-core-1.0.7.jar
 SLF4J=slf4j-1.7.0/slf4j-api-1.7.0.jar
 
-javac -cp $SLF4J:$LOGBACK:$JACKSON -d build `find src -name "*.java" -type f -print`
+javac -cp $SLF4J:$LOGBACK:$JACKSON -d build -encoding UTF-8 `find src -name "*.java" -type f -print`
 cp src/logback.xml build
