871366196e5f9a255a4fb09c53c61074b9ff41b7
Jesse Hallio <jessegit@jesse.iki.fi>
2012-09-23 21:56:36 +0300

    Clean up imports.

--- ShallowGreen/src/shallowgreen/message/Message.java
+++ ShallowGreen/src/shallowgreen/message/Message.java
@@ -1,24 +1,22 @@
 package shallowgreen.message;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 
 @JsonTypeInfo(
 	use=JsonTypeInfo.Id.NAME,
 	include=JsonTypeInfo.As.PROPERTY,
 	property="msgType")
 @JsonSubTypes({
-	@Type(value=JoinMessage.class, name="join"),
-	@Type(value=RequestDuelMessage.class, name="requestDuel"),
-	@Type(value=JoinedMessage.class, name="joined"),
-	@Type(value=GameStartedMessage.class, name="gameStarted"),
-	@Type(value=GameIsOverMessage.class, name="gameIsOver"),
-	@Type(value=GameIsOnMessage.class, name="gameIsOn"),
-	@Type(value=ChangeDirMessage.class, name="changeDir"),
-	@Type(value=ErrorMessage.class, name="error")
+	@JsonSubTypes.Type(value=JoinMessage.class, name="join"),
+	@JsonSubTypes.Type(value=RequestDuelMessage.class, name="requestDuel"),
+	@JsonSubTypes.Type(value=JoinedMessage.class, name="joined"),
+	@JsonSubTypes.Type(value=GameStartedMessage.class, name="gameStarted"),
+	@JsonSubTypes.Type(value=GameIsOverMessage.class, name="gameIsOver"),
+	@JsonSubTypes.Type(value=GameIsOnMessage.class, name="gameIsOn"),
+	@JsonSubTypes.Type(value=ChangeDirMessage.class, name="changeDir"),
+	@JsonSubTypes.Type(value=ErrorMessage.class, name="error")
 })
 public abstract class Message {
 
